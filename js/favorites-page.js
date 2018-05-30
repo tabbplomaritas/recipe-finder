41291a430b6dfd7e233c48c0e591ecd955e29e3a
AnthonyMarc23 <anthony@adaptivelite.com>
2018-05-30 16:10:41 -0400

    preparing the favorites-page.js for functionality

--- js/favorites-page.js
+++ js/favorites-page.js
@@ -1,11 +1,25 @@
 "use strict";
 
 const favoritesPage = {
-  template: `
+  bindings: {
+    favorite: "<"
+  },
 
+  template: `
+  <div class="recipeThumbs" ng-repeat="item in $ctrl.favorites">
+    <h1>{{item.recipe.label}}</h1>
+    <img src="{{ item.recipe.image }}" >
+    <p>Ingredients: </p>
+  </div>
   `,
   controller: ["RecipeRequest", function(RecipeRequest) {
     const vm = this;
+
+    vm.newRecipe = {
+      recipeTitle: vm.result.data.hits.recipe.label,
+      recipeImg: vm.result.data.hits.recipe.img,
+      recipeIngred: vm.result.data.hits.recipe.img
+    }
 
   }]
 };
