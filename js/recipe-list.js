ff50252ba95e6697ded9f7da60db575f33526e17
tabbatharenea@gmail.com <tabbatharenea@gmail.com>
2018-05-31 16:01:54 -0400

    fixed merge errors

--- js/recipe-list.js
+++ js/recipe-list.js
@@ -8,13 +8,8 @@ const recipeList = {
      
       <div class="recipeThumbs" ng-click="$ctrl.sendRecipeDetails(item);" ng-repeat="item in $ctrl.result.data.hits" item="item">
         <div class="starCont">  
-<<<<<<< HEAD
-          <h1>{{item.recipe.label}}</h1>
-          <i class="material-icons" ng-click="$ctrl.addToFavorite(item); $event.stopPropagation();">grade</i>
-=======
           <h4 class="recipelabel">{{item.recipe.label}}</h4>
-          <i class="material-icons" ng-click="$ctrl.addToFavorite(item);">grade</i>
->>>>>>> eb486ac30b4e09cdb403e4bd770d9fd6dc02222a
+          <i class="material-icons" ng-click="$ctrl.addToFavorite(item); $event.stopPropagation();">grade</i>
         </div>
         <img src="{{ item.recipe.image }}" >
         </p>
