1b40f930efc826bde08a95f3286d20e40f890a45
AnthonyMarc23 <anthony@adaptivelite.com>
2018-06-01 13:15:50 -0400

    Added comments

--- js/recipe-details.js
+++ js/recipe-details.js
@@ -4,10 +4,10 @@ const recipeDetails = {
    
     template: `
         <div class="recipe">
-        <div class="sectionHeader"
-            <h2>{{$ctrl.clickedRecipe.recipe.label}}</h2>
-            <a href="javascript:void(0);" ng-click="$ctrl.goBack();"><i class="material-icons">cancel</i></a>
-        </div>
+            <div class="sectionHeader"
+                <h2>{{$ctrl.clickedRecipe.recipe.label}}</h2>
+                <a href="javascript:void(0);" ng-click="$ctrl.goBack();"><i class="material-icons">cancel</i></a>
+            </div>
             <img src="{{ $ctrl.clickedRecipe.recipe.image }}">
             <h4>Prep Time: <spanclass="recipeSmallFont">{{ $ctrl.clickedRecipe.recipe.totalTime }}</spanclass> Minutes</h4>
             <h4 class="ingredients">Ingredients:</h4>
@@ -17,15 +17,17 @@ const recipeDetails = {
             <p><a href="{{ $ctrl.clickedRecipe.recipe.url }}" target="_blank">Click here</a> for instructions.</p>
         </div>
     `,
+
     controller: ["RecipeRequest", function(RecipeRequest) {
         const vm = this;
-        vm.clickedRecipe = RecipeRequest.viewDetails();
-        console.log(vm.clickedRecipe);
 
+        // grab the recipe from our service
+        vm.clickedRecipe = RecipeRequest.viewDetails();
+        
+        // set up a goBack function for our "X"
         vm.goBack = () => { 
             window.history.back();
         };
-        
     }]
 }
 
